bb9412827c8e3ffa206460c2ceb8ea35a01d8b2d
TheoLendler <tl42546@srcs.org>
2022-01-13 15:31:13 -0800

    Lab 3 complete

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Animator animator;
+    private Rigidbody rb;
+    private Vector3 moveDirection;
+    private Quaternion rotation;
+    private bool isWalking;
+
+    [SerializeField] private float turnSpeed = 20f;
+
     // Start is called before the first frame update
     void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // Animator event
+    private void OnAnimatorMove()
     {
         
     }
